1a784eb433f4fed30ec775c94eda22fce3765345
May jiraphon <jiraphon.ne@bitnance.com>
2021-11-09 16:01:58 +0700

    fixed bg tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
